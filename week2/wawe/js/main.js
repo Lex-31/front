fd9486d62c6c2fbe891c628f483a577efed3da34
Aleks Work <57226635+Lex-31@users.noreply.github.com>
2022-04-18 19:00:51 +0300

    DZ7

--- week2/wawe/js/main.js
+++ week2/wawe/js/main.js
@@ -1,7 +1,14 @@
 $(function () {
-  var mixer = mixitup('.gallery__content');
   $('.slider-blog__inner').slick({
     dots: true,
     arrows: false,
   });
+
+  //------header menu-------
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  var mixer = mixitup('.gallery__content');
+
 });

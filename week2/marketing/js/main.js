3b1831e909d178871ad76ee2f2a3488ad4e71699
Aleks Work <57226635+Lex-31@users.noreply.github.com>
2022-05-02 01:36:25 +0300

    DZ8

--- week2/marketing/js/main.js
+++ week2/marketing/js/main.js
@@ -4,6 +4,14 @@ $(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow right"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
   });
   //------header menu-------
   $('.menu__btn').on('click', function () {

4fda0d7813d46d23eacaecf4f6f000c305333c54
Aleks Work <lex_31@mail.ru>
2022-08-05 01:50:33 +0300

    DZ10

--- week2/marketing/js/main.js
+++ week2/marketing/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
+  $(".menu a, .go-top").on("click", function (e) { //.menu a, .go-top - ссылки якорные с такими классами будут плавно скроллить
+    e.preventDefault(); //отменяем стандартную обработку нажатия по ссылке
+    var id = $(this).attr('href'), //забираем идентификатор бока с атрибута href
+
+      top = $(id).offset().top; //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+    $('body,html').animate({ scrollTop: top }, 1500); //анимируем переход на расстояние - top за 1500 мс
+  });
+
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow left"></button>',
@@ -13,9 +22,10 @@ $(function () {
       },
     ]
   });
-  //------header menu-------
-  $('.menu__btn').on('click', function () {
-    $('.header__top-inner').toggleClass('header__top-inner--active');
+
+  //------header menu------- открывать/закрывать меню на мобильнй версии
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.header__top-inner').toggleClass('header__top-inner--active'); //при нажатии кнопки .menu__btn будет меняться класс менюшки, при нажатии кнопки ".menu a" будет скрываться менюшка
   });
 
   var mixer = mixitup('.portfolio__content');

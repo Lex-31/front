0863bddea1f304f26584547298f8b323d1399ea7
Lex-31 <lex_31@mail.ru>
2023-01-01 18:37:18 +0100

    DZ14

--- marcho/gulpfile.js
+++ marcho/gulpfile.js
@@ -30,9 +30,10 @@ function styles() {
 }
 
 function scripts() {
-  return src([
+  return src([  //собираем все необходимые скрипты
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js')) //новое имя результирующего файла

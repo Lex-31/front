4d68b851d68bbb6f72a8ba3b5f0f95140507ff9c
Lex-31 <lex_31@mail.ru>
2023-01-23 01:41:12 +0100

    DZ19.1

--- marcho/app/js/main.js
+++ marcho/app/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
 
+  $('.product-tabs__top-item').on('click', function (e) { //переключение вкладок, связываем табы с вкладками
+    e.preventDefault(); //отменяем стандартое действие при клике по ссылке
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  })
+
   $('.product-slide__thumb').slick({ //список превью слайдов
     asNavFor: '.product-slide__big', //связываем два слайдера
     focusOnSelect: true,

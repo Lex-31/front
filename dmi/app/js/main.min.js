baa6d85cf10580383e5e4e0c9b3160042e664aaf
Lex-31 <lex_31@mail.ru>
2023-11-12 20:05:58 +0100

    fix

--- dmi/app/js/main.min.js
+++ dmi/app/js/main.min.js
@@ -1,6 +1,7 @@
 //Кнопка меню
 const btn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu__list');
+const menuItems = document.querySelectorAll('.menu__list-link');
 
 btn.addEventListener('click', () => { // скрол страницы вверх
   const scrollUp = () => { // прокрутка страницы вверх
@@ -14,8 +15,14 @@ btn.addEventListener('click', () => { // скрол страницы вверх
   const menuTopAdd = () => { // доб/уд класс, фиксирует от прокрутки
     menu.classList.toggle('menu__list--active'); // добавить/удалить новый класс 
     if (menu.classList.contains('menu__list--active')) { //если есть новый класс
+      menuItems.forEach((menuItem) => { //перебираем все ссылки в меню
+        menuItem.onclick = function () { //вешаем им событие клика по ссылке в меню
+          menu.classList.remove('menu__list--active'); //удалить новый класс - закрыть меню
+          document.body.style.overflow = ''; //разфиксируем body для перемещения
+        };
+      });
       document.body.style.overflow = 'hidden'; //фиксируем body от перемещения
-    } else {
+    } else { //разфиксируем body для перемещения
       document.body.style.overflow = '';
     }
   }
@@ -35,52 +42,46 @@ btn.addEventListener('click', () => { // скрол страницы вверх
 });
 
 //Video overlay
-const overlay = document.querySelector('.featured-right__video-overlay');
-const video = document.querySelector('.featured-right__video-inner video');
-video.volume = 0.3; //громкость на 50% выставляем
-overlay.onclick = function () { //когда кликаем на overlay
-  video.play(); //запускаем видео
-  this.classList.add('featured-right__video-overlay--hidden'); //обращаемся к overlay, скрываем overlay
-};
+if (window.location.pathname === '/index.html' || window.location.pathname === '/about.html') { //выполняем код только если находимся на странице
+  console.log(window.location.pathname);
+  const overlay = document.querySelector('.featured-right__video-overlay');
+  const clip = document.querySelector('.featured-right__video-inner video');
+  clip.volume = 0.3; //громкость на 50% выставляем
+  overlay.onclick = function () { //когда кликаем на overlay
+    clip.play(); //запускаем видео
+    this.classList.add('featured-right__video-overlay--hidden'); //обращаемся к overlay, скрываем overlay
+  };
 
-video.onclick = function () { //когда кликаем на видео
-  this.pause(); //ставим на паузу видео
-  overlay.classList.remove('featured-right__video-overlay--hidden'); //показываем overlay
+  clip.onclick = function () { //когда кликаем на видео
+    this.pause(); //ставим на паузу видео
+    overlay.classList.remove('featured-right__video-overlay--hidden'); //показываем overlay
+  }
 }
 
-
 //Кнопки на фотке
-const buttons = document.querySelectorAll('.program__item'); // Получите все элементы с классом
-buttons.forEach((button) => { // Проходимся по каждой кнопке и добавляем обработчик события клика
-  button.addEventListener('click', () => {
-    //активными могут быть несколько конопк
-    /* if (button.classList.contains('program__item--active')) {  // Проверяем, есть ли у кнопки класс-модификатор
-       button.classList.remove('program__item--active'); // Если есть, удалите его
-     } else {
-       button.classList.add('program__item--active'); // Если нет, добавьте его
-     } */
+if (window.location.pathname === '/index.html') { //выполняем код только если находимся на странице
+  const buttons = document.querySelectorAll('.program__item'); // Получите все элементы с классом
+  buttons.forEach((button) => { // Проходимся по каждой кнопке и добавляем обработчик события клика
+    button.addEventListener('click', () => {
+      //активными могут быть несколько конопк
+      /* if (button.classList.contains('program__item--active')) {  // Проверяем, есть ли у кнопки класс-модификатор
+         button.classList.remove('program__item--active'); // Если есть, удалите его
+       } else {
+         button.classList.add('program__item--active'); // Если нет, добавьте его
+       } */
 
-    //активной может быть только одна кнопка
-    buttons.forEach(function (btn) {
-      btn.classList.remove('program__item--active');
+      //активной может быть только одна кнопка
+      buttons.forEach(function (btn) {
+        btn.classList.remove('program__item--active');
+      });
+      button.classList.add('program__item--active');
+
     });
-    button.classList.add('program__item--active');
 
+    const top = button.getAttribute('data-top'); // Получаем значение top из data-атрибута
+    button.style.top = top + 'px'; // Установите индивидуальное значение top для текущей кнопки
   });
-
-  const top = button.getAttribute('data-top'); // Получаем значение top из data-атрибута
-  button.style.top = top + 'px'; // Установите индивидуальное значение top для текущей кнопки
-});
-
-//форма изменение акцентного цвета
-const color = document.getElementById('color');
-const colorNumber = document.querySelector('.color');
-const elementProp = document.querySelector('body');
-color.addEventListener('change', () => {
-  elementProp.style.setProperty('--accent-color', color.value); //изменяем переменную CSS
-  colorNumber.innerHTML = color.value;
-});
-
+}
 
 
 //Калькулятор ИМТ
@@ -115,6 +116,68 @@ color.addEventListener('change', () => {
 //   };
 // });
 
+
+// Pop-up фото увеличение
+if (window.location.pathname === '/about.html') { //выполняем код только если находимся на странице
+  const certItems = document.querySelectorAll('.cert__item'); //берем все фото
+  certItems.forEach((certItem) => { //перебираем их с помощью цикла
+    certItem.addEventListener('click', function () { //навешиваем на каждую событие клика
+      this.classList.toggle('pop-up'); //добавлять-удалять класс при клике 
+      if (this.classList.contains('pop-up')) { //проверяем наличие класса, если есть класс...
+        disablePageScroll(); //блокируем прокрутку фоновой страницы
+      } else { //если нет класса...
+        enablePageScroll(); //разблокировка прокрутки фоновой страницы
+      }
+    });
+  });
+}
+
+
+//слайдер
+if (window.location.pathname === '/index.html') { //выполняем код только если находимся на странице
+  const swiper = new Swiper(".swiper", { //плагин swiper вешаем на класс div элемента 
+    loop: true, //бесконечный слайд
+    slidesPerView: 1, // сколько слайдов в активной области
+    spaceBetween: 20, //расстояние между слайдами
+    allowTouchMove: true, //разрешить листать свайпом
+    centeredSlides: true, //по центру активный слайд
+    pagination: { //параметры пагинации
+      el: '.swiper-pagination', //пагинацию вешаем на класс div элемента
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      1180: { // when window width is >= 1180px
+        slidesPerView: 1.9, // сколько слайдов в активной области
+        spaceBetween: 30, //расстояние между слайдами
+      },
+      900: {
+        slidesPerView: 1.5, // сколько слайдов в активной области
+        spaceBetween: 20, //расстояние между слайдами
+      },
+      740: {
+        slidesPerView: 1.2, // сколько слайдов в активной области
+        spaceBetween: 20, //расстояние между слайдами
+      },
+      680: {
+        allowTouchMove: false, //запретить листать свайпом
+      },
+      500: {
+        slidesPerView: 1.5, // сколько слайдов в активной области
+        spaceBetween: 30, //расстояние между слайдами
+      },
+      400: {
+        slidesPerView: 1.2, // сколько слайдов в активной области
+      },
+    }
+  });
+}
+
+
 // Модальное окно checkbox
 const modal = document.getElementById('modal');
 const checkbox = document.getElementById('subscribe');
@@ -148,19 +211,6 @@ cancelButton.addEventListener('click', () => {
   enablePageScroll(); //разблокировка прокрутки фоновой страницы
 });
 
-// Pop-up фото увеличение
-const certItems = document.querySelectorAll('.cert__item'); //берем все фото
-certItems.forEach((certItem) => { //перебираем их с помощью цикла
-  certItem.addEventListener('click', function () { //навешиваем на каждую событие клика
-    this.classList.toggle('pop-up'); //добавлять-удалять класс при клике 
-    if (this.classList.contains('pop-up')) { //проверяем наличие класса, если есть класс...
-      disablePageScroll(); //блокируем прокрутку фоновой страницы
-    } else { //если нет класса...
-      enablePageScroll(); //разблокировка прокрутки фоновой страницы
-    }
-  });
-});
-
 
 // Form согласие на обработку
 const form = document.getElementById("form");
@@ -179,44 +229,11 @@ form.addEventListener("submit", (event) => {
 });
 
 
-//слайдер
-const swiper = new Swiper(".swiper", { //плагин swiper вешаем на класс div элемента 
-  loop: true, //бесконечный слайд
-  slidesPerView: 1, // сколько слайдов в активной области
-  spaceBetween: 20, //расстояние между слайдами
-  allowTouchMove: true, //разрешить листать свайпом
-  centeredSlides: true, //по центру активный слайд
-  pagination: { //параметры пагинации
-    el: '.swiper-pagination', //пагинацию вешаем на класс div элемента
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  breakpoints: {
-    1180: { // when window width is >= 1180px
-      slidesPerView: 1.9, // сколько слайдов в активной области
-      spaceBetween: 30, //расстояние между слайдами
-    },
-    900: {
-      slidesPerView: 1.5, // сколько слайдов в активной области
-      spaceBetween: 20, //расстояние между слайдами
-    },
-    740: {
-      slidesPerView: 1.2, // сколько слайдов в активной области
-      spaceBetween: 20, //расстояние между слайдами
-    },
-    680: {
-      allowTouchMove: false, //запретить листать свайпом
-    },
-    500: {
-      slidesPerView: 1.5, // сколько слайдов в активной области
-      spaceBetween: 30, //расстояние между слайдами
-    },
-    400: {
-      slidesPerView: 1.2, // сколько слайдов в активной области
-    },
-  }
+//Форма изменение акцентного цвета
+const color = document.getElementById('color');
+const colorNumber = document.querySelector('.color');
+const elementProp = document.querySelector('body');
+color.addEventListener('change', () => {
+  elementProp.style.setProperty('--accent-color', color.value); //изменяем переменную CSS
+  colorNumber.innerHTML = color.value;
 });
